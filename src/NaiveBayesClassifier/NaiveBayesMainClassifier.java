6ab1ca80d0148c4ec3bb29baf5aa00d53a709b0f
CharlesJeremy <mulemi.charles29@gmail.com>
2015-06-30 16:53:31 -0700

    more edits on the naivebayesclassifier

--- src/NaiveBayesClassifier/NaiveBayesMainClassifier.java
+++ src/NaiveBayesClassifier/NaiveBayesMainClassifier.java
@@ -18,6 +18,21 @@ public class NaiveBayesMainClassifier implements ClassifierConstants{
 	
 	public void train(){
 		 currentFrequencyTable = new  int[2][NUMBER_OF_BOOK_COLUMNS];
+		 for(int i = 0; i < NUMBER_OF_STUDENT_ROWS; i ++){
+			 for(int j = 0; j < NUMBER_OF_BOOK_COLUMNS; j ++){	 
+		    	 if (trainingData[i][j] == 0){
+					 currentFrequencyTable[0][j] ++;
+				 }else{
+					 currentFrequencyTable[1][j] ++;
+				 }
+			}	 
+		}
+		//Laplace adjust
+		 for(int i = 0; i < 2; i ++){
+			 for(int j = 0; j < NUMBER_OF_BOOK_COLUMNS; j ++){	 
+				 currentFrequencyTable[i][j] ++;
+			 }
+		}
 	}
 	
 	
