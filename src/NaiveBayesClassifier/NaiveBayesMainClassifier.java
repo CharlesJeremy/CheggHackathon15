29664f424d2c8be4c479a58ac310205fb3799459
CharlesJeremy <mulemi.charles29@gmail.com>
2015-06-30 15:18:45 -0700

    creating the putline of the project

---
+++ src/NaiveBayesClassifier/NaiveBayesMainClassifier.java
@@ -0,0 +1,19 @@
+package NaiveBayesClassifier;
+
+import java.util.List;
+
+import ProjectWideResources.ClassifierConstants;
+
+
+
+public class NaiveBayesMainClassifier implements ClassifierConstants{
+	private int[NUMBER_OF_BOOKS][]currentFrequencyTable;
+	public NaiveBayesMainClassifier(List<List<Integer>> trainingData){
+		
+	}
+	
+	public double getTutorProbability (List<Integer> booksBoughtByStudent) {
+		return 0.0;
+	}
+
+}
---
+++ src/ProjectWideResources/ClassifierConstants.java
@@ -0,0 +1,8 @@
+package ProjectWideResources;
+
+public interface ClassifierConstants {
+	NUMBER_OF_BOOK_ROWS = 10;
+	NUMBER_OF_STUDENTS = 10;
+	NUMBER_OF_TUTORS = 20;
+
+}
