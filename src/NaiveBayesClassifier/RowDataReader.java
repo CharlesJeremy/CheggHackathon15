5ce5990cf387f245c752ef98f309fadc68cb0965
John Pericich Jr <jpericich@sfml-jpericich.chegg.com>
2015-06-30 17:17:08 -0700

    Updated the file reader

--- src/NaiveBayesClassifier/RowDataReader.java
+++ src/NaiveBayesClassifier/RowDataReader.java
@@ -16,10 +16,13 @@ public class RowDataReader implements ClassifierConstants{
 		int[][] data = new int[NUMBER_OF_STUDENT_ROWS][NUMBER_OF_BOOK_COLUMNS + 1];
 		try {
 			BufferedReader rd = new BufferedReader(new FileReader(filename));
+			for (int i = 0; i < 2; i++) {
+				rd.readLine();
+			}
 			for (int rowIndex = 0; rowIndex < NUMBER_OF_STUDENT_ROWS; rowIndex++) {
-				String[] input = rd.readLine().split("[ ]+");
+				String input = rd.readLine();
 				for (int columnIndex = 0; columnIndex < NUMBER_OF_BOOK_COLUMNS + 1; columnIndex++) {
-					data[rowIndex][columnIndex] = Integer.parseInt(input[columnIndex]);
+					data[rowIndex][columnIndex] = Integer.parseInt(input.charAt(columnIndex) + "");
 				}
 			}
 			rd.close();
