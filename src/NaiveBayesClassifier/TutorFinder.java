5bec3f815b96748cdb797a9d55c3c96f09f5f36a
CharlesJeremy <mulemi.charles29@gmail.com>
2015-07-01 09:01:21 -0700

    need to update everything

---
+++ src/NaiveBayesClassifier/TutorFinder.java
@@ -0,0 +1,25 @@
+package NaiveBayesClassifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ProjectWideResources.ClassifierConstants;
+import Tutor.Tutor;
+
+public class TutorFinder implements ClassifierConstants {
+    private static final String TUTOR1 = "Charles";
+    private static final String TUTOR2 = "Nicole";
+    private static final String TUTOR3 = "John";
+    private static final String TUTOR4 = "Deepak";
+    private static final String TUTOR5 = "Nathaniel";
+    private static final String TUTOR6 = "Gerrit";
+    
+	
+	public TutorFinder(){
+    	
+    }
+	
+	public List<Tutor> getSortedTutorSuggestions(){
+		return new ArrayList<Tutor>();
+	}
+}
