c88e570bef0a2860245ff5efd42deed24d4f3ea4
CharlesJeremy <mulemi.charles29@gmail.com>
2015-06-30 21:22:08 -0700

    Classifier done

--- src/NaiveBayesClassifier/NaiveBayesMainClassifier.java
+++ src/NaiveBayesClassifier/NaiveBayesMainClassifier.java
@@ -11,18 +11,26 @@ public class NaiveBayesMainClassifier implements ClassifierConstants{
 	
 	private List<Student> studentsTrainingData;
 	private int[][] currentFrequencyTable;
+	private double  probabilityUsedTutorDenominator;
+	private double  probabilityUsedNumerator;
 
 	public NaiveBayesMainClassifier(List<Student> studentstTrainingData){
 		this.studentsTrainingData =  studentstTrainingData;;
 		currentFrequencyTable = new int[2][NUMBER_OF_BOOK_COLUMNS * 2];
+		probabilityUsedTutorDenominator = 0;
+		probabilityUsedNumerator = 0;
 	}
 	
 	
 	public void train(){
 		 currentFrequencyTable = new  int[2][NUMBER_OF_BOOK_COLUMNS];
 		 for(int i = 0; i < NUMBER_OF_STUDENT_ROWS; i ++){
+			 Student studentObject = studentsTrainingData.get(i);
+			 probabilityUsedTutorDenominator ++;
+			 if(studentObject.getUsedTutor() == 1){
+				 probabilityUsedNumerator ++;	
+			 }
 			 for(int j = 0; j < NUMBER_OF_BOOK_COLUMNS; j ++){	
-				 Student studentObject = studentsTrainingData.get(i);
 		    	 if (studentObject.getPurchasedBook(j) == 0){
 		    		 if(studentObject.getUsedTutor() == 0){
 		    			 currentFrequencyTable[0][j * 2] ++;
@@ -50,7 +58,19 @@ public class NaiveBayesMainClassifier implements ClassifierConstants{
 	
 	
 	public double getTutorProbability (Student  willStudentuseTutor) {
-		return 0.0;
+		double probabilityWill =  probabilityUsedNumerator / probabilityUsedTutorDenominator;
+		//double probabilityWont =  (probabilityUsedTutorDenominator - probabilityUsedNumerator) / probabilityUsedTutorDenominator;
+		
+		for(int i = 0; i < NUMBER_OF_BOOK_COLUMNS; i ++){
+			double probabilityGivenBookNumerator = currentFrequencyTable[1][i * 2];
+			double probabilityGivenBookDenominator =  currentFrequencyTable[1][i * 2] + currentFrequencyTable[1][(i * 2) + 1];
+			probabilityWill *= (probabilityGivenBookNumerator / probabilityGivenBookDenominator);
+			
+			/*double probabilityGivenNotBookNumerator = currentFrequencyTable[0][i * 2];
+			double probabilityGivenNotBookDenominator =  currentFrequencyTable[0][i * 2] + currentFrequencyTable[0][(i * 2) + 1];
+			probabilityWont *= (probabilityGivenNotBookNumerator / probabilityGivenNotBookDenominator);*/
+		}		
+		return probabilityWill;
 	}
     
 	
